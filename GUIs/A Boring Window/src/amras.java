232b749a609919e1501bb6c52236694e793370d1
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-14 20:45:40 +0300

    A Boring Window

---
+++ GUIs/A Boring Window/src/amras.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by KAAN on 14/05/2016.
+ */
+public class amras extends JFrame{
+    public static void main(String[] args){
+
+        JFrame f = new amras();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1024,768);
+        f.setVisible(true);
+
+    }
+}
