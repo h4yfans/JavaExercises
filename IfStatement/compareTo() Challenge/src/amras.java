70ebfcdd8cbbc13e495c9e07a99696dc1f711c90
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-14 23:57:01 +0300

    compareTo

---
+++ IfStatement/compareTo() Challenge/src/amras.java
@@ -0,0 +1,15 @@
+/**
+ * Created by KAAN on 14/05/2016.
+ */
+public class amras {
+    public static void main(String[] args){
+
+        System.out.print("Comparing \"axe\" with \"dog\" produces ");
+        int i = "axe".compareTo("dog");
+        System.out.println(i);
+
+        System.out.print("Comparing \"applebee's\" with \"apple\" produces ");
+        System.out.println( "applebee's".compareTo("apple") );
+
+    }
+}
