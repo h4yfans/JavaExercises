d212c91e9c84d5074335ec84ab33158e44f7a874
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-14 18:55:54 +0300

    Weekday Name

---
+++ IfStatement/WeekdayName/src/amras.java
@@ -0,0 +1,58 @@
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/**
+ * Created by KAAN on 14/05/2016.
+ */
+public class amras {
+
+    public static String weekdayName(int weekday) {
+        String result = "";
+
+        if (weekday == 1)
+            result = "Sunday";
+        else if (weekday == 2)
+            result = "Monday";
+        else if (weekday == 3)
+            result = "Tuesday";
+        else if (weekday == 4)
+            result = "Wednesday";
+        else if (weekday == 5)
+            result = "Thursday";
+        else if (weekday == 6)
+            result = "Friday";
+        else if (weekday == 7)
+            result = "Saturday";
+        else if (weekday == 0)
+            result = "Saturday";
+        else
+            result = "error";
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Weekday 1: " + weekdayName(1));
+        System.out.println("Weekday 2: " + weekdayName(2));
+        System.out.println("Weekday 3: " + weekdayName(3));
+        System.out.println("Weekday 4: " + weekdayName(4));
+        System.out.println("Weekday 5: " + weekdayName(5));
+        System.out.println("Weekday 6: " + weekdayName(6));
+        System.out.println("Weekday 7: " + weekdayName(7));
+        System.out.println("Weekday 0: " + weekdayName(0));
+        System.out.println();
+        System.out.println("Weekday 43: " + weekdayName(43));
+        System.out.println("Weekday 17: " + weekdayName(17));
+        System.out.println("Weekday -1: " + weekdayName(-1));
+
+        Scanner input = new Scanner(System.in);
+
+        GregorianCalendar cal = new GregorianCalendar();
+        int dow = cal.get(GregorianCalendar.DAY_OF_WEEK);
+
+        System.out.println("\nToday is a " + weekdayName(dow) + "!");
+
+
+    }
+}
