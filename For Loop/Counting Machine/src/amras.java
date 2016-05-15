40099e4c4e7cb2f71b7a97becf607a36a45b237c
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-15 18:05:10 +0300

    Count

---
+++ For Loop/Counting Machine/src/amras.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by KAAN on 15/05/2016.
+ */
+public class amras {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+
+        for(int i = 1; i < a+1 ; i++){
+        System.out.println(i);
+        }
+
+    }
+}
